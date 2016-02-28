f4d5c5573ccbbdf3c84504a80a47e2c8f4467c85
Ruben Vereecken <rubenvereecken@gmail.com>
2016-02-28 21:56:23 +0100

    Added package.json with the downside that my dotfiles now require node

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,4 +1,6 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH"
+# Node programs I track in my dotfiles package.json
+export PATH="$HOME/.dotfiles/node_modules/.bin:$PATH"
 
 # These are for working with CUDA
 export PATH="/usr/local/cuda/bin:$PATH"
