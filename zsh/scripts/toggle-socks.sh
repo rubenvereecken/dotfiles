fb5efbc13f6b1d0b4b6fac0135f2ec73426b71b8
Ruben Vereecken <rubenvereecken@gmail.com>
2018-11-19 15:57:33 +0000

    More general socks script

--- zsh/scripts/toggle-socks.sh
+++ zsh/scripts/toggle-socks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # SOCKS5 proxy on port 8123
 PROXY_PORT=8123
+PROXY_ADDR="socks://localhost:$PROXY_PORT/"
 
 nc -z localhost $PROXY_PORT
 is_up=$?
@@ -8,7 +9,9 @@ is_up=$?
 if [[ $is_up != 0 ]]; then
   ssh -D $PROXY_PORT -fCqN marston
   gsettings set org.gnome.system.proxy mode manual
-  notify-send "SOCKS5 proxy @ localhost:$PROXY_PORT"
+  gsettings set org.gnome.system.proxy.socks host 'localhost'
+  gsettings set org.gnome.system.proxy.socks port $PROXY_PORT
+  notify-send "SOCKS5 proxy @ $PROXY_ADDR"
 else
   fuser -k "$PROXY_PORT/tcp"
   gsettings set org.gnome.system.proxy mode none
